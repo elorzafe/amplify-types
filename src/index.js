89b2109c1e59284bb96814fb7fed5fea51241c82
Jim Blanchard <jablanch@amazon.com>
2023-03-02 15:47:38 -0600

    Removed type files.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Amplify = void 0;
-exports.Amplify = {
-    configure: function (config) {
-        throw new Error("Function not implemented.");
-    }
-};
