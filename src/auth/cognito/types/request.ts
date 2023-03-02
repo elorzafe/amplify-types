faac1490dc0d59d263f6f70be3b429f23284fed8
Jim Blanchard <jablanch@amazon.com>
2023-03-02 15:42:56 -0600

    Removed all types for review.

--- src/auth/cognito/types/request.ts
+++
@@ -1,8 +0,0 @@
-import { AuthProvider } from "../../types/models"
-import { FederateToIdentityPoolOptions } from "./options"
-
-export type FederateToIdentityPoolRequest = {
-    providerToken:string,
-    authProvider: AuthProvider
-    options?: FederateToIdentityPoolOptions
-  }
